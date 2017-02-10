f2af081ea8d984876430c1e6b33135054fcce8e4
Sho <sfaizi1@outlook.com>
2017-02-09 16:41:03 -0800

    add user model to user route

--- routes/user.js
+++ routes/user.js
@@ -5,4 +5,20 @@ router.get('/new', function (req, res, next) {
   res.render('user/new');
 });
 
+router.post('/', function (req, res, next) {
+  var user = new User({
+    name: req.body.name,
+    lastName: req.body.lastName,
+    email: req.body.email
+  });
+
+  user.save(function (err, user) {
+    if(err) {
+      res.render('user/new', { errors: err})
+    } else {
+      res.end('User Created');
+    }
+  });
+});
+
 module.exports = router;
