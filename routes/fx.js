5c242cb50134a0c536973cb825bc13e2fc2b6b24
Sho <sfaizi1@outlook.com>
2017-02-21 19:32:46 -0800

    receive data with JSON and convert to string

--- routes/fx.js
+++ routes/fx.js
@@ -6,9 +6,23 @@ var latestUrl = 'http://api.fixer.io/latest';
 
 router.get('/', function(req, res, next) {
   axios.get(`${latestUrl}`)
-    .then(res => {
-      var a = res.data.rates;
+    .then(response => {
+      // var arr = [];
+      // var parsed = JSON.parse(response.data.rates);
+      // for(var x in parsed){
+      //   arr.push(parsed[x]);
+      // }
+      // console.log(arr);
+
+      // var a = response.data.rates;
+      var a = JSON.parse(response.data.rates);
+      // var temp = [];
+      // Object.keys(a).forEach(function(key){
+      //   // console.log(key, a[key]);
+      //   temp[key] = a[key];
+      // });
       console.log(a);
+
       res.render('fx/index', {data: a});
     })
 });
