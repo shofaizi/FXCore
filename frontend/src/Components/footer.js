50e8fb5157b95fec2c82d334527d9d89c78e0812
Sho <sfaizi1@outlook.com>
2017-02-27 18:36:47 -0800

    add footer and add styling

--- frontend/src/Components/footer.js
+++ frontend/src/Components/footer.js
@@ -1,25 +1,35 @@
 import React from 'react';
-import '../footer.css';
+import '../css/footer.css';
 
-export default class Header extends React.Component {
+export default class Footer extends React.Component {
   render () {
     return (
-      <div className='navbar'>
-        <div className='navbar-logo'>LOGO</div>
-          <ul className='navbar-items'>
-            <li>
-              <a>FXCore</a>
-            </li>
-            <li>
-              <a>FXNews</a>
-            </li>
-            <li>
-              <a className='signin'>Sign In</a>
-            </li>
-            <li>
-              <a className='gettingStarted'>Get Started</a>
-            </li>
-          </ul>
+      <div className='footer'>
+        <div className='leftColumn'>
+          <div className='logo'></div>
+            <p className='footerParagraph'>
+              FXCore is currently in BETA 2.0.
+              Subscribe to our latest newsletter for updates and our weekly curated
+              Sunday News Channel
+            </p>
+          <div>
+            <input className='email' placeholder='Enter email'></input>
+          </div>
+        </div>
+        <div className='centerColumn'>
+          <h5>FXCore</h5>
+          <a href='#'>Showcase FXCore</a>
+          <a href='#'>Legal/Privacy</a>
+          <a href='#'>FAQ</a>
+          <p>© 2017 FXCore Inc all right reserved</p>
+        </div>
+        <div className='rightColumn'>
+          <h5>Follow Us</h5>
+          <a href='#'>Twitter</a>
+          <a href='#'>Facebook</a>
+          <a href='#'>Github</a>
+          <a href='#'>Contact Us</a>
+        </div>
       </div>
     )
   }
--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './css/App.css';
 import Header from './Components/header';
+import Footer from './Components/footer';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Header />
+        <Footer />
       </div>
     );
   }
