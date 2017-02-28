7334e1b767934d686a96bd2ad4841792997ce097
Sho <sfaizi1@outlook.com>
2017-02-27 16:59:03 -0800

    add styling to navbar

--- frontend/src/Components/header.js
+++ frontend/src/Components/header.js
@@ -4,29 +4,23 @@ import '../header.css';
 export default class Header extends React.Component {
   render () {
     return (
-      <nav className='navbar navbar-default'>
-        <div className='container-fluid'>
-          <div className='navbar-header'>
-            <a className='navbar-brand' href='#'>
-              <img alt=""/>
-            </a>
-            <ul className='navbar-items'>
-              <li>
-                <a>FXCore</a>
-              </li>
-              <li>
-                <a>FXNews</a>
-              </li>
-              <li>
-                <a>Sign In</a>
-              </li>
-              <li>
-                <a>Get Started</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+      <div className='navbar'>
+        <div className='navbar-logo'>LOGO</div>
+          <ul className='navbar-items'>
+            <li>
+              <a>FXCore</a>
+            </li>
+            <li>
+              <a>FXNews</a>
+            </li>
+            <li>
+              <a className='signin'>Sign In</a>
+            </li>
+            <li>
+              <div className='gettingStarted'>Get Started</div>
+            </li>
+          </ul>
+      </div>
     )
   }
 }
