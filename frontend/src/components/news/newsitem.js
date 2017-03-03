4a270f8a2d2cab6786dc767e395554006d079adb
Sho <sfaizi1@outlook.com>
2017-03-02 18:49:02 -0800

    add state to bookmark button and add css for toggling on/off bookmark button

--- frontend/src/components/news/newsitem.js
+++ frontend/src/components/news/newsitem.js
@@ -5,12 +5,18 @@ export default class NewsItem extends React.Component {
     super(props)
     this.state = {
       liked: false,
+      bookmarked: false
     }
     this.onLikeChange = this.onLikeChange.bind(this)
+    this.onBookMarkChange = this.onBookMarkChange.bind(this)
   }
 
   onLikeChange() {
     this.setState({liked: !this.state.liked});
+  }
+
+  onBookMarkChange() {
+    this.setState({bookmarked: !this.state.bookmarked});
   }
 
   render () {
@@ -31,10 +37,10 @@ export default class NewsItem extends React.Component {
           </div>
           <div className='icons'>
             <span onClick={this.onLikeChange}>
-              <i id='heart' className="fa fa-heart-o fa-2x" aria-hidden="true" style={{color: this.state.liked ? 'red' : 'white'}}></i>
+              <i id='heart' className="fa fa-heart fa-2x" aria-hidden="true" style={{color: this.state.liked ? 'red' : 'white'}}></i>
             </span>
-            <span>
-              <i className="fa fa-bookmark-o fa-2x" aria-hidden="true"></i>
+            <span onClick={this.onBookMarkChange}>
+              <i className="fa fa-bookmark fa-2x" aria-hidden="true" style={{color: this.state.bookmarked ? 'black' : 'white'}}></i>
             </span>
           </div>
         </div>
