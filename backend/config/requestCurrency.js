c76ad0089ee2b53757b669e8e899c7044ff7ab02
Sho <sfaizi1@outlook.com>
2017-03-08 22:30:21 -0800

    ***fixed get data and save to database

--- backend/config/requestCurrency.js
+++ backend/config/requestCurrency.js
@@ -1,33 +1,39 @@
   var Currency = require('../models/currency');
+  var mongoose = require('mongoose');
   var axios    = require('axios');
-  var apiURL = 'http://apilayer.net/api/historical?access_key=8321275ba97c3742b8d48a772540a2e4&date=2017-02-20'
+  var apiURL = 'http://apilayer.net/api/historical?&date=2017-02-20'
 
-var atry = function() {
-  axios.get(`${apiURL}`)
-  .then(res => {
-    var quotes = res.data.quotes;
-    for(var key in quotes) {
-      if(quotes.hasOwnProperty(key)){
-        var curr = new Currency({
-          base: res.data.source,
-          date: res.data.date,
-          currency: key,
-          value: quotes[key]
-        })
-        console.log(curr)
-        curr.save(function (err, curr) {
-          if(err) {
-            return err;
-          } else {
-            return curr;
-          }
-        })
+  mongoose.connect("mongodb://localhost/fxcore_db", function(err, res){
+    if(err) throw err;
+    console.log("Connected to mongodb");
+  });
+
+  var atry = function() {
+    axios.get(`${apiURL}`)
+    .then(res => {
+      var quotes = res.data.quotes;
+      for(var key in quotes) {
+        if(quotes.hasOwnProperty(key)){
+          var currency = new Currency({
+            base: res.data.source,
+            date: res.data.date,
+            currency: key,
+            value: quotes[key]
+          })
+          console.log(currency)
+          currency.save(function (err, currency) {
+            if(err) {
+              return err;
+            } else {
+              return currency;
+            }
+          })
+        }
       }
-    }
-  })
-  .catch(function(err) {
-    return err;
-  })
-}
+    })
+    .catch(function(err) {
+      return err;
+    })
+  }
 
-atry();
+  atry();
--- backend/models/currency.js
+++ backend/models/currency.js
@@ -1,12 +1,12 @@
 var mongoose = require("mongoose"),
     Schema   = mongoose.Schema;
 
-var CurrencySchema = new Schema({
-  base: {type: String, trim: true, required:true},
+var Currency = new Schema({
+  base: {type: String, trim: true, required: true},
   date: {type: String, trim: true, required: true},
   currency: {type: String, trim: true, required: true},
   value: {type: Number, trim: true, required: true}
 },{timestamp: true});
 
 
-module.exports = mongoose.model("Currency", CurrencySchema)
+module.exports = mongoose.model("Currency", Currency)
